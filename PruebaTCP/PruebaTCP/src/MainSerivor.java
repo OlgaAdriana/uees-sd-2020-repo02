6f05a509013d6915d0f4ec2295c98e84a357601d
OlgaAdriana <67136337+OlgaAdriana@users.noreply.github.com>
2020-10-13 11:35:53 -0500

    version02 tarea4

---
+++ PruebaTCP/PruebaTCP/src/MainSerivor.java
@@ -0,0 +1,23 @@
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose
+ */
+public class MainSerivor {
+     public static void main(String[] args) throws IOException
+    {
+        Servidor serv = new Servidor(); //Se crea el servidor
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+    
+}
---
+++ PruebaTCP/PruebaTCP/src/mainCliente.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose
+ */
+public class mainCliente {
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+}
